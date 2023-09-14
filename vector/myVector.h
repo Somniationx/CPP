6356ab9c0cfbd841517d33a3d49ad62269fc76af
Somniationx <629701713@qq.com>
2023-09-14 18:05:22 +0800

    update

--- vector/myVector.h
+++ vector/myVector.h
@@ -3,53 +3,107 @@
 
 namespace Somn {
 
+	/// <summary>
+	/// Template class for a dynamic array-like container.
+	/// </summary>
 	template<class T>
 	class myVector {
 	public:
 		typedef T* iterator;           // Iterator type for non-constant access
 		typedef const T* const_iterator; // Iterator type for constant access
 
-		// Default constructor
+		/// <summary>
+		/// Default constructor
+		/// </summary>
 		myVector();
 
-		// Add an element to the back of the vector
+		/// <summary>
+		/// Add an element to the back of the vector
+		/// </summary>
+		/// <param name="val">The value to be added.</param>
 		void push_back(const T& val);
 
-		// Reserve space for a specified number of elements
+		/// <summary>
+		/// Reserve space for a specified number of elements
+		/// </summary>
+		/// <param name="n">The number of elements to reserve memory for.</param>
 		void reserve(size_t n);
 
-		// Get the current size of the vector
+		/// <summary>
+		/// Get the current size of the vector
+		/// </summary>
+		/// <returns>The number of elements in the vector.</returns>
 		size_t size() const;
 
-		// Get the current capacity of the vector
+		/// <summary>
+		/// Get the current capacity of the vector
+		/// </summary>
+		/// <returns>The storage capacity of the vector.</returns>
 		size_t capacity() const;
 
-		// Get an iterator pointing to the beginning of the vector
+		/// <summary>
+		/// Get an iterator pointing to the beginning of the vector
+		/// </summary>
+		/// <returns>An iterator pointing to the first element.</returns>
 		iterator begin();
 
-		// Get a constant iterator pointing to the beginning of the vector
+		/// <summary>
+		/// Get a constant iterator pointing to the beginning of the vector
+		/// </summary>
+		/// <returns>A constant iterator pointing to the first element.</returns>
 		const_iterator begin() const;
 
-		// Get a constant iterator pointing to the end of the vector
+		/// <summary>
+		/// Get a constant iterator pointing to the end of the vector
+		/// </summary>
+		/// <returns>A constant iterator pointing just past the last element.</returns>
 		const_iterator end() const;
 
-		// Get an iterator pointing to the end of the vector
+		/// <summary>
+		/// Get an iterator pointing to the end of the vector
+		/// </summary>
+		/// <returns>An iterator pointing just past the last element.</returns>
 		iterator end();
 
-		// Access an element in the vector by index
+		/// <summary>
+		/// Access an element in the vector by index
+		/// </summary>
+		/// <param name="pos">The index of the element to access.</param>
+		/// <returns>A reference to the element at the specified index.</returns>
 		T& operator[](size_t pos);
 
-		// Check if the vector is empty.
+		/// <summary>
+		/// Check if the vector is empty.
+		/// </summary>
+		/// <returns>True if the vector is empty, false otherwise.</returns>
 		bool empty() const;
 
-		// Resize the vector to contain 'n' elements.
+		/// <summary>
+		/// Resize the vector to contain 'n' elements.
+		/// </summary>
+		/// <param name="n">The new size of the vector.</param>
+		/// <param name="val">The value used to fill new elements if 'n' is greater than the current size.</param>
 		void resize(size_t n, T val = T());
 
+		/// <summary>
+		/// Remove the last element from the vector.
+		/// </summary>
 		void pop_back();
 
-		void insert(iterator pos, const T& val);
+		/// <summary>
+		/// Insert an element at a specified position in the vector.
+		/// </summary>
+		/// <param name="pos">The iterator indicating the position to insert the element.</param>
+		/// <param name="val">The value to be inserted.</param>
+		/// <returns>An iterator pointing to the inserted element.</returns>
+		iterator insert(iterator pos, const T& val);
 
-		void erase(iterator pos);
+		/// <summary>
+		/// Erase an element at a specified position in the vector.
+		/// </summary>
+		/// <param name="pos">The iterator indicating the position of the element to be erased.</param>
+		/// <returns>An iterator pointing to the element following the erased element.</returns>
+		iterator erase(iterator pos);
 
 	private:
 		iterator _start;          // Pointer to the start of the vector
@@ -57,12 +111,13 @@ namespace Somn {
 		iterator _end_of_storage; // Pointer to the end of the allocated memory
 	};
 
-
+	// Default constructor
 	template<class T>
 	inline myVector<T>::myVector() : _start(nullptr), _finish(nullptr), _end_of_storage(nullptr) {}
 
+	// Add an element to the back of the vector
 	template<class T>
-	inline void myVector<T>::push_back(const T& val) 
+	inline void myVector<T>::push_back(const T& val)
 	{
 		// Check if space needs to be expanded or opened up when the capacity is full or if it is an initial vector.
 		if (_finish == _end_of_storage) {
@@ -75,8 +130,9 @@ namespace Somn {
 		_finish++;
 	}
 
+	// Reserve space for a specified number of elements
 	template<class T>
-	inline void myVector<T>::reserve(size_t newCapacity) 
+	inline void myVector<T>::reserve(size_t newCapacity)
 	{
 		// This function cannot perform capacity reduction.
 		if (newCapacity > capacity()) {
@@ -88,7 +144,7 @@ namespace Somn {
 				memcpy(tempSpace, _start, sizeof(T) * size());
 				delete[] _start;
 			}
-			
+
 			// Preserve the size value to avoid errors in the subsequent size() function.
 			size_t len = size();
 
@@ -99,9 +155,10 @@ namespace Somn {
 		}
 	}
 
+	// Get the current size of the vector
 	template<class T>
-	inline size_t myVector<T>::size() const{
-		if (_start == nullptr || _finish == nullptr) 
+	inline size_t myVector<T>::size() const {
+		if (_start == nullptr || _finish == nullptr)
 		{
 			// Handling the case of an empty object
 			return 0;
@@ -109,6 +166,7 @@ namespace Somn {
 		return _finish - _start;
 	}
 
+	// Get the current capacity of the vector
 	template<class T>
 	inline size_t myVector<T>::capacity() const
 	{
@@ -119,6 +177,7 @@ namespace Somn {
 		return _end_of_storage - _start;
 	}
 
+	// Get an iterator pointing to the beginning of the vector
 	template<class T>
 	inline typename myVector<T>::iterator myVector<T>::begin()
 	{
@@ -126,6 +185,7 @@ namespace Somn {
 		return _start;
 	}
 
+	// Get a constant iterator pointing to the beginning of the vector
 	template<class T>
 	inline typename myVector<T>::const_iterator myVector<T>::begin() const
 	{
@@ -133,6 +193,7 @@ namespace Somn {
 		return _start;
 	}
 
+	// Get a constant iterator pointing to the end of the vector
 	template<class T>
 	inline typename myVector<T>::const_iterator myVector<T>::end() const
 	{
@@ -140,7 +201,7 @@ namespace Somn {
 		return _finish;
 	}
 
-
+	// Get an iterator pointing to the end of the vector
 	template<class T>
 	inline typename myVector<T>::iterator myVector<T>::end()
 	{
@@ -148,6 +209,7 @@ namespace Somn {
 		return _finish;
 	}
 
+	// Access an element in the vector by index
 	template<class T>
 	inline T& myVector<T>::operator[](size_t pos)
 	{
@@ -156,6 +218,7 @@ namespace Somn {
 		return _start[pos];
 	}
 
+	// Check if the vector is empty.
 	template<class T>
 	inline bool myVector<T>::empty() const
 	{
@@ -164,6 +227,7 @@ namespace Somn {
 		return _start == _finish;
 	}
 
+	// Resize the vector to contain 'n' elements.
 	template<class T>
 	inline void myVector<T>::resize(size_t n, T val)
 	{
@@ -187,6 +251,7 @@ namespace Somn {
 		}
 	}
 
+	// Remove the last element from the vector.
 	template<class T>
 	inline void myVector<T>::pop_back()
 	{
@@ -194,12 +259,13 @@ namespace Somn {
 		_finish--;
 	}
 
+	// Insert an element at a specified position in the vector.
 	template<class T>
-	inline void myVector<T>::insert(iterator pos, const T& val)
+	inline typename myVector<T>::iterator myVector<T>::insert(iterator pos, const T& val)
 	{
 		assert(pos >= _start && pos <= _finish);
 		size_t len = pos - _start;
-	
+
 		if (_finish == _end_of_storage) {
 			size_t newCapacity = capacity() == 0 ? 4 : capacity() * 2;
 			reserve(newCapacity);
@@ -213,9 +279,12 @@ namespace Somn {
 		}
 		*pos = val;
 		_finish++;
+		return pos;
 	}
+
+	// Erase an element at a specified position in the vector.
 	template<class T>
-	inline void myVector<T>::erase(iterator pos)
+	inline typename myVector<T>::iterator myVector<T>::erase(iterator pos)
 	{
 		assert(!empty());
 		assert(pos >= _start);
@@ -227,5 +296,6 @@ namespace Somn {
 			cur++;
 		}
 		_finish--;
+		return pos;
 	}
 }
--- vector/test.cpp
+++ vector/test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "myVector.h" // Include your header file based on your filename and path
 
-int main() {
+void test_vector1() {
     using namespace Somn;
 
     // Create an empty myVector object
@@ -25,6 +25,78 @@ int main() {
 
     // Test the operator[] function
     std::cout << "Element at index 3: " << myVec[3] << std::endl;
+}
 
+void test_vector2() {
+    using namespace Somn;
+
+    myVector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3); 
+    v.push_back(4); 
+
+    myVector<int>::iterator it = std::find(v.begin(), v.end(), 3);
+
+    if (it != v.end()) {
+        v.erase(it);   // Erase the element pointed to by it
+        // After erasing, it is invalidated and should not be used.
+    }
+
+    // Now, you can't use it1 here because it's invalidated.
+    // *it = 5; or std::cout << *it << std::endl;
+
+    std::cout << "myVector elements: ";
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        std::cout << *it << " ";  // Print the elements in myVector
+    }
+}
+
+void test_vector3() {
+    using namespace Somn;
+
+    myVector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(2);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(6);
+
+    myVector<int>::iterator it = v.begin();  // Initialize an iterator pointing to the beginning of the vector
+
+    // wrong writing
+    while (it != v.end()) {  // Loop until the iterator reaches the end of the vector
+        if (*it % 2 == 0) {  // Check if the element pointed to by the iterator is even
+            v.erase(it);     // Erase the even element from the vector
+            // Warning: After erasing, the iterator 'it' is invalidated and should not be used.
+        }
+        else {
+            it++;  // Move the iterator to the next element
+        }
+    }
+    
+    // Correct writing
+    //while (it != v.end()) {  // Loop until the iterator reaches the end of the vector
+    //    if (*it % 2 == 0) {  // Check if the element pointed to by the iterator is even
+    //        it = v.erase(it);  // Erase the even element from the vector and get the new iterator
+    //    }
+    //    else {
+    //        ++it;  // Move the iterator to the next element
+    //    }
+    //}
+
+    // WARNING: You should not use 'it' here as it may be invalidated by the erase operation.
+
+    std::cout << "myVector elements: ";
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        std::cout << *it << " ";  // Print the elements in myVector
+    }
+}
+
+int main() {
+    test_vector3();
     return 0;
 }
