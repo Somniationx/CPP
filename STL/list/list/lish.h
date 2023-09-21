39326edec96f13bfd5165bfd521a756d49f19692
Somniationx <629701713@qq.com>
2023-09-21 21:25:02 +0800

    Perfect list

--- STL/list/list/lish.h
+++ STL/list/list/lish.h
@@ -14,13 +14,14 @@ namespace Somn {
          * @brief Constructor to initialize the node with data.
          * @param val The data to be stored in the node.
          */
-        list_node(const T& val);
+        list_node(const T& val) : _next(nullptr), _pre(nullptr), _data(val) {}
     };
 
     // Iterator for the linked list
-    template<class T>
+    template<class T, class Ref, class Ptr>
     struct __list_iterator {
         typedef list_node<T> node;
+        typedef __list_iterator<T, Ref, Ptr> Self;
 
         node* _pnode;       // Pointer to the current node
 
@@ -28,70 +29,62 @@ namespace Somn {
          * @brief Constructor to initialize the iterator with a node.
          * @param ptr A pointer to the node to be associated with this iterator.
          */
-        __list_iterator(node* ptr);
+        __list_iterator(node* ptr) : _pnode(ptr) {}
 
         /**
          * @brief Overloaded dereference operator (*) to access node data.
          * @return A reference to the data stored in the current node.
          */
-        T& operator*();
+        Ref operator*() {
+            return _pnode->_data;
+        }
+
+        Ptr operator->() {
+            return &_pnode->_data;
+        }
 
         /**
          * @brief Overloaded pre-increment operator (++) to move to the next node.
          * @return A reference to the updated iterator.
          */
-        __list_iterator<T>& operator++();
+        Self& operator++() {
+            _pnode = _pnode->_next;
+            return *this;
+        }
+
+        Self operator++(int) {
+            Self temp(*this);
+            _pnode = _pnode->_next;
+            return temp;
+        }
 
         /**
          * @brief Overloaded pre-decrement operator (--) to move to the previous node.
          * @return A reference to the updated iterator.
          */
-        __list_iterator<T>& operator--();
+        Self& operator--() {
+            _pnode = _pnode->_pre;
+            return *this;
+        }
+
+        Self operator--(int) {
+            Self temp(*this);
+            _pnode = _pnode->_pre;
+            return temp;
+        }
+
+        bool operator!=(const Self& it) const {
+            return _pnode != it._pnode;
+        }
 
         /**
          * @brief Overloaded inequality operator (!=) to compare two iterators.
          * @param it The iterator to be compared with the current iterator.
          * @return True if the iterators are not equal, false otherwise.
          */
-        bool operator!=(const __list_iterator<T>& it);
-    };
-
-    template<class T>
-    struct __list_const_iterator {
-        typedef list_node<T> node;
-
-        node* _pnode;       // Pointer to the current node
-
-        /**
-         * @brief Constructor to initialize the iterator with a node.
-         * @param ptr A pointer to the node to be associated with this iterator.
-         */
-        __list_const_iterator(node* ptr);
-
-        /**
-         * @brief Overloaded dereference operator (*) to access node data.
-         * @return A reference to the data stored in the current node.
-         */
-        const T& operator*() const;
-
-        /**
-         * @brief Overloaded pre-increment operator (++) to move to the next node.
-         * @return A reference to the updated iterator.
-         */
-        __list_const_iterator<T>& operator++();
-
-        /**
-         * @brief Overloaded pre-decrement operator (--) to move to the previous node.
-         * @return A reference to the updated iterator.
-         */
-        __list_const_iterator<T>& operator--();
-
-        /**
-         * @brief Overloaded inequality operator (!=) to compare two iterators.
-         * @param it The iterator to be compared with the current iterator.
-         * @return True if the iterators are not equal, false otherwise.
-         */
-        bool operator!=(const __list_const_iterator<T>& it);
+        bool operator!=(const __list_iterator<T, Ref>& it) {
+            return _pnode != it._pnode;
+        }
     };
 
     // Doubly linked list class
@@ -99,43 +92,85 @@ namespace Somn {
     class list {
     public:
         typedef list_node<T> node;
-        typedef __list_iterator<T> iterator;
-        typedef __list_const_iterator<T> const_iterator;
+        typedef __list_iterator<T, T&, T*> iterator;
+        typedef __list_iterator<T, const T&, const T*> const_iterator;
 
         /**
          * @brief Initialize an empty list.
          */
-        void empty_initialize();
-
-        /**
-         * @brief Constructor to initialize an empty list with a dummy head node.
-         */
-        list();
+        list() {
+            empty_initialize();
+        }
 
         /**
          * @brief Copy constructor.
          * @param lt The list to be copied.
          */
-        list(const list& lt);
+        list(const list& lt) {
+            empty_initialize();
+
+            /* for (const auto& val : lt) {
+                 push_back(val);
+             }*/
+
+            empty_initialize();
+            list<T> temp(lt.begin(), lt.edn());
+            swap(temp);
+        }
+
+        template<class InputIterator>
+        list(InputIterator first, InputIterator last) {
+            empty_initialize();
+
+            while (first != last) {
+                push_back(*first);
+                first++;
+            }
+        }
+
+        void swap(list<T>& lt) {
+            std::swap(_head, lt._head);
+            std::swap(_size, lt._size);
+        }
 
         /**
          * @brief Assignment operator.
          * @param lt The list to be assigned.
          * @return A reference to the assigned list.
          */
-        list& operator=(const list<T>& lt);
+        list& operator=(const list<T>& lt) {
+            if (this != &lt) {
+                this->clear();
+
+                for (const auto& val : lt) {
+                    this->push_back(val);
+                }
+            }
+            return *this;
+        }
+
+        list<T>& operator=(list<T> lt) {
+            swap(lt);
+            return *this;
+        }
 
         /**
          * @brief Add an element to the end of the list.
          * @param val The value to be added to the end of the list.
          */
-        void push_back(const T& val);
+        void push_back(const T& val) {
+            // Insert a new node with the given value at the end of the list.
+            insert(end(), val);
+        }
 
         /**
          * @brief Add an element to the front of the list.
          * @param val The value to be added to the front of the list.
          */
-        void push_front(const T& val);
+        void push_front(const T& val) {
+            // Insert a new node with the given value at the beginning of the list.
+            insert(begin(), val);
+        }
 
         /**
          * @brief Insert an element at a specified position in the list.
@@ -143,260 +178,178 @@ namespace Somn {
          * @param val The value to be inserted.
          * @return An iterator pointing to the inserted element.
          */
-        iterator insert(iterator pos, const T& val);
+        iterator insert(iterator pos, const T& val) {
+            // Insert a new node with the given value at the specified position.
+            node* new_node = new node(val);
+            node* cur = pos._pnode;
+            node* prev = cur->_pre;
+
+            prev->_next = new_node;
+            new_node->_pre = prev;
+            new_node->_next = cur;
+            cur->_pre = new_node;
+
+            _size++;
+
+            return iterator(new_node);
+        }
 
         /**
          * @brief Erase an element at a specified position in the list.
          * @param pos The iterator pointing to the position of the element to be erased.
          * @return An iterator pointing to the element following the erased element.
          */
-        iterator erase(iterator pos);
+        iterator erase(iterator pos) {
+            // Erase the node at the specified position.
+            assert(pos != end());
+
+            node* prev = pos._pnode->_pre;
+            node* next = pos._pnode->_next;
+
+            prev->_next = next;
+            next->_pre = prev;
+
+            delete pos._pnode;
+
+            _size--;
+
+            return iterator(next);
+        }
 
         /**
          * @brief Remove the first element of the list.
          */
-        void pop_front();
+        void pop_front() {
+            // Remove the first element of the list.
+            erase(begin());
+        }
 
         /**
          * @brief Remove the last element of the list.
          */
-        void pop_back();
+        void pop_back() {
+            // Remove the last element of the list.
+            erase(end()._pnode->_pre);
+        }
 
         /**
          * @brief Get an iterator pointing to the beginning of the list.
          * @return An iterator pointing to the first element in the list.
          */
-        iterator begin();
+        iterator begin() {
+            return iterator(_head->_next);
+        }
 
         /**
          * @brief Get a const iterator pointing to the beginning of the list.
          * @return A const iterator pointing to the first element in the list.
          */
-        const_iterator begin() const;
+        const_iterator begin() const {
+            return const_iterator(_head->_next);
+        }
 
         /**
          * @brief Get an iterator pointing to the end of the list.
          * @return An iterator pointing one past the end of the list.
          */
-        iterator end();
+        iterator end() {
+            return iterator(_head);
+        }
 
         /**
          * @brief Get a const iterator pointing to the end of the list.
          * @return A const iterator pointing one past the end of the list.
          */
-        const_iterator end() const;
+        const_iterator end() const {
+            return const_iterator(_head);
+        }
 
         /**
          * @brief Clear the list, removing all elements.
          */
-        void clear();
+        void clear() {
+            iterator cur = begin();
+            while (cur != end()) {
+                cur = erase(cur);
+            }
+        }
+
+        size_t size() const {
+            return _size;
+        }
+
+        bool empty() const {
+            return _size == 0;
+        }
 
         /**
          * @brief Destructor to clean up the list.
          */
-        ~list();
+        ~list() {
+            clear();
+            delete _head;
+            _head = nullptr;
+        }
 
     private:
         node* _head; // Pointer to the dummy head node of the list
+        size_t _size;
+
+        inline void empty_initialize() {
+            _head = new node(T());
+            _head->_next = _head;
+            _head->_pre = _head;
+            _size = 0;
+        }
     };
 
+    // Constant Iterator for the linked list
     template<class T>
-    inline void list<T>::empty_initialize()
-    {
-        _head = new node(T());
-        _head->_next = _head;
-        _head->_pre = _head;
-    }
+    struct __list_const_iterator {
+        typedef list_node<T> node;
 
-    template<class T>
-    inline list<T>::list() {
-        empty_initialize();
-    }
+        const node* _pnode;  // Pointer to the current node
 
-    template<class T>
-    inline list<T>::list(const list& lt)
-    {
-        empty_initialize();
+        /**
+         * @brief Constructor to initialize the constant iterator with a node.
+         * @param ptr A pointer to the node to be associated with this constant iterator.
+         */
+        __list_const_iterator(const node* ptr) : _pnode(ptr) {}
 
-        for (const auto& val : lt) {
-            push_back(val);
+        /**
+         * @brief Overloaded dereference operator (*) to access node data.
+         * @return A constant reference to the data stored in the current node.
+         */
+        const T& operator*() const {
+            return _pnode->_data;
         }
-    }
 
-    template<class T>
-    inline list<T>& list<T>::operator=(const list<T>& lt)
-    {
-        if (this != &lt) {
-            this->clear();
-
-            for (const auto& val : lt) {
-                this->push_back(val);
-            }
+        /**
+         * @brief Overloaded pre-increment operator (++) to move to the next node.
+         * @return A reference to the updated constant iterator.
+         */
+        __list_const_iterator<T>& operator++() {
+            _pnode = _pnode->_next;
+            return *this;
         }
-        return *this;
-    }
 
-    template<class T>
-    inline void list<T>::push_back(const T& val) {
-        // Insert a new node with the given value at the end of the list.
-        insert(end(), val);
-    }
-
-    template<class T>
-    inline void list<T>::push_front(const T& val)
-    {
-        // Insert a new node with the given value at the beginning of the list.
-        insert(begin(), val);
-    }
-
-    template<class T>
-    inline typename list<T>::iterator list<T>::insert(iterator pos, const T& val)
-    {
-        // Insert a new node with the given value at the specified position.
-        node* new_node = new node(val);
-        node* cur = pos._pnode;
-        node* prev = cur->_pre;
-
-        prev->_next = new_node;
-        new_node->_pre = prev;
-        new_node->_next = cur;
-        cur->_pre = new_node;
-
-        return iterator(new_node);
-    }
-
-    template<class T>
-    inline typename list<T>::iterator list<T>::erase(iterator pos)
-    {
-        // Erase the node at the specified position.
-        assert(pos != end());
-
-        node* prev = pos._pnode->_pre;
-        node* next = pos._pnode->_next;
-
-        prev->_next = next;
-        next->_pre = prev;
-
-        delete pos._pnode;
-
-        return iterator(next);
-    }
-
-    template<class T>
-    inline void list<T>::pop_front()
-    {
-        // Remove the first element of the list.
-        erase(begin());
-    }
-
-    template<class T>
-    inline void list<T>::pop_back()
-    {
-        // Remove the last element of the list.
-        erase(end()._pnode->_pre);
-    }
-
-    template<class T>
-    inline typename list<T>::iterator list<T>::begin() {
-        return iterator(_head->_next);
-    }
-
-    template<class T>
-    inline typename list<T>::const_iterator list<T>::begin() const
-    {
-        return const_iterator(_head->_next);
-    }
-
-    template<class T>
-    inline typename list<T>::iterator list<T>::end() {
-        return iterator(_head);
-    }
-
-    template<class T>
-    inline typename list<T>::const_iterator list<T>::end() const
-    {
-        return const_iterator(_head);
-    }
-
-    template<class T>
-    inline void list<T>::clear()
-    {
-        iterator cur = begin();
-        while (cur != end()) {
-            cur = erase(cur);
+        /**
+         * @brief Overloaded pre-decrement operator (--) to move to the previous node.
+         * @return A reference to the updated constant iterator.
+         */
+        __list_const_iterator<T>& operator--() {
+            _pnode = _pnode->_pre;
+            return *this;
         }
-    }
 
-    template<class T>
-    inline list<T>::~list()
-    {
-        clear();
-        delete _head;
-        _head = nullptr;
-    }
-
-    template<class T>
-    inline list_node<T>::list_node(const T& val) {
-        _next = _pre = nullptr;
-        _data = val;
-    }
-
-    template<class T>
-    inline __list_iterator<T>::__list_iterator(node* ptr)
-        : _pnode(ptr) {}
-
-    template<class T>
-    inline T& __list_iterator<T>::operator*() {
-        return _pnode->_data;
-    }
-
-    template<class T>
-    inline __list_iterator<T>& __list_iterator<T>::operator++() {
-        _pnode = _pnode->_next;
-        return *this;
-    }
-
-    template<class T>
-    inline __list_iterator<T>& __list_iterator<T>::operator--()
-    {
-        _pnode = _pnode->_pre;
-        return *this;
-    }
-
-    template<class T>
-    inline bool __list_iterator<T>::operator!=(const __list_iterator<T>& it) {
-        return _pnode != it._pnode;
-    }
-
-    template<class T>
-    inline __list_const_iterator<T>::__list_const_iterator(node* ptr)
-        :_pnode(ptr)
-    {}
-
-    template<class T>
-    inline const T& __list_const_iterator<T>::operator*() const
-    {
-        return _pnode->_data;
-    }
-
-    template<class T>
-    inline __list_const_iterator<T>& __list_const_iterator<T>::operator++()
-    {
-        _pnode = _pnode->_next;
-        return *this;
-    }
-
-    template<class T>
-    inline __list_const_iterator<T>& __list_const_iterator<T>::operator--()
-    {
-        _pnode = _pnode->_pre;
-        return *this;
-    }
-
-    template<class T>
-    inline bool __list_const_iterator<T>::operator!=(const __list_const_iterator<T>& it)
-    {
-        return _pnode != it._pnode;
-    }
+        /**
+         * @brief Overloaded inequality operator (!=) to compare two constant iterators.
+         * @param it The constant iterator to be compared with the current constant iterator.
+         * @return True if the constant iterators are not equal, false otherwise.
+         */
+        bool operator!=(const __list_const_iterator<T>& it) const {
+            return _pnode != it._pnode;
+        }
+    };
 
 }
